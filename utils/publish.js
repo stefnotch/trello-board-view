309635815215fdad075c510ba1442f0eed19ff57
Stefnotch <brandmairstefan@gmail.com>
2020-04-13 15:50:44 +0200

    Improve deploying

--- utils/publish.js
+++ utils/publish.js
@@ -1,10 +1,6 @@
 var ghpages = require("gh-pages");
 
-ghpages.publish(
-  ".",
-  { src: ["index.html", "dist/**/*"], history: false },
-  (err) => {
-    if (err) console.error(err);
-    else console.log("Published to GitHub");
-  }
-);
+ghpages.publish("dist", { history: false }, (err) => {
+  if (err) console.error(err);
+  else console.log("Published to GitHub");
+});
